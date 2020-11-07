0e163820261bef83e76a94d9bc35fabb16ab9740
Christian <christian_haller@gmx.de>
2020-11-06 21:39:05 -0500

    initial

---
+++ Polls_App/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+#add namespace of these URLs -> only used for Polls_App
+app_name = 'Polls_App'
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
