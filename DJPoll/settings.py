79e57bfd4b4e1bb62ccbca0b79e887e71761aef3
Christian <christian_haller@gmx.de>
2020-11-06 23:55:07 -0500

    replace settings

--- DJPoll/settings.py
+++ DJPoll/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f_qc2dd*=s3#f@m6jv&s@)eoeev85+gm(@1ct@@qp$gxk9_753'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
